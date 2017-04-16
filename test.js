c5b05022ba676b4287cd487184c8d1e4ff009ae5
Heterotroph <r.akhtyamov@live.com>
2017-04-17 00:47:49 +0300

    Added chart data process methods

--- test.js
+++ test.js
@@ -30,11 +30,13 @@ function testComponents() {
   	var style = {
         background: {color: "#00AAFF", alpha: 0.1},
         axis: {thickness: 2, color: "#00FFFF", alpha: 0.8},
-        grid: {thickness: 0.5, color: "#00FFFF", alpha: 0.5}
+        grid: {thickness: 0.5, color: "#00FFFF", alpha: 0.5},
+        chart: {thickness: 3, radius: 4, color: "#000000", alpha: 0.8}
     };
     var chart = stage.addChild(new tgc.Chart(size, segments, style));
     chart.y = 50;
     chart.x = 50;
+    chart.append([300, 300, 800, 1000, 700, 880, 1000, 800, 700, 500]);
       			
     createjs.Ticker.on("tick", stage);
 }
--- tgc-lib.js
+++ tgc-lib.js
@@ -9,7 +9,7 @@ var tgc = {};
      *  background: {color: "#000000", alpha: 0.5},
      *  axis: {thickness: 3, color: "#FF0000", alpha: 1},
      *  grid: {thickness: 1, color: "#00FFFF", alpha: 1},
-     *  chart: {thicness: 2, radius: 4, color: "000000", alpha: 0.9}
+     *  chart: {thickness: 3, radius: 4, color: "#000000", alpha: 0.8}
      * }
      */ 
     function Chart(size, segments, style) {
@@ -19,8 +19,8 @@ var tgc = {};
         this.segments = segments;
         this.style = style;
         
+        this.data = [];
         this.maxCountY = 0;
-        this.currentOffset = 0;
         
         this.backgroundShape = this.addChild(new createjs.Shape());
         this.axisShape = this.addChild(new createjs.Shape());
@@ -35,7 +35,27 @@ var tgc = {};
     //PUBLIC METHODS
     
     p.append = function(data) {
-        var mult = this.segments.multiplier;
+        var totalData = this.data.concat(data);
+        var freeSegmentsCount = Math.max(this.segments.countX - this.data.length, 0);
+        var stepX = this.size.width / this.segments.countX;
+        var offsetX = 0;
+        
+        if (freeSegmentsCount) {
+            offsetX = this.data.length * stepX;
+            if (this.data.length) {
+                data.unshift(this.data[this.data.length - 1]);
+                offsetX --;
+            }
+        } else {
+            this.chartShape.graphics.clear();
+        }
+        this._drawChart(offsetX, stepX, data, this.style.chart);
+        this.data = totalData;
+    };
+    
+    p.clear = function() {
+        this.segments = [];
+        this.chartShape.graphics.clear();
     };
     
     p.updateStyle = function() {
@@ -50,30 +70,43 @@ var tgc = {};
         this.updateStyle();
     };
     
-    p._drawSegment= function(aX, aY, bX, bY, style) {
+    p._drawChart = function(offsetX, stepX, data, style) {
+        var mult = this.segments.multiplier;
+        var aX, aY, bX, bY;
+        aX = offsetX;
+        aY = data[0] * mult;
+        for (var i = 0; i < data.length - 1; i++) {
+            bX = offsetX + stepX * i;
+            bY = data[i + 1] * mult;
+            this._drawSegment(aX, aY, bX, bY, style);
+            aX = bX;
+            aY = bY;
+        }
+    };
+    
+    p._drawSegment = function(aX, aY, bX, bY, style) {
         var graphics = this.chartShape.graphics;
         
         graphics.setStrokeStyle(style.thickness, "round");
         graphics.beginStroke(style.color);
-        graphics.moveTo(aX, aY).lineTo(bX, bY);
+        graphics.moveTo(aX, this.size.height - aY).lineTo(bX, this.size.height - bY);
         graphics.endStroke();
         
-        this._drawPoint(aX, aY, "standart");
-        this._drawPoint(bX, bY, "standart");
+        this._drawPoint(bX, bY, "standart", style);
     };
     
     p._drawPoint = function(x, y, type, style) {
         var graphics = this.chartShape.graphics;
         switch(type) {
         case "selected":
-            graphics.setStrokeStyle(style.thickness, "butt");
+            graphics.setStrokeStyle(style.thickness, "round");
             graphics.beginStroke(style.color);
-            graphics.drawCircle(x, y, style.radius);
+            graphics.drawCircle(x, this.size.height - y, style.radius);
             graphics.endStroke();
             break;
         case "standart":
             graphics.beginFill(style.color);
-            graphics.drawCircle(x, y, style.radius);
+            graphics.drawCircle(x, this.size.height - y, style.radius);
             graphics.endFill();
             break;
         default:
