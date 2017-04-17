4eb5e96ece461e10eec983ae2edd5f012f257377
Heterotroph <r.akhtyamov@live.com>
2017-04-18 01:41:09 +0300

    Dimension refactoring

--- test.js
+++ test.js
@@ -26,23 +26,23 @@ function testComponents() {
     stage.enableMouseOver();
       			
     var size = {width: 1000, height: 400};
-  	var segments = {multiplier: 0.2, countX: 25, minCountY: 10};
+  	var point = {width: 50, height: 1};
   	var style = {
         background: {color: "#00AAFF", alpha: 0.1},
         axis: {thickness: 2, color: "#00FFFF", alpha: 0.8},
-        grid: {thickness: 0.5, color: "#00FFFF", alpha: 0.5},
+        grid: {thickness: 0.5, color: "#00FFFF", alpha: 0.5, width: 2, height: 100},
         chart: {thickness: 3, radius: 4, color: "#000000", alpha: 0.8}
     };
-    var chart = stage.addChild(new tgc.Chart(size, segments, style));
+    var chart = stage.addChild(new tgc.StreamingChart(size, point, style));
     chart.y = 50;
     chart.x = 50;
-    chart.append([1200, 950, 1000, 400, 100, 0, 0, 300, 1000]);
+    chart.append([300, 390, 370, 200, 220, 100, 0, 50, 300]);
       			
     createjs.Ticker.on("tick", function() {
         stage.update();
     });
     
     setInterval(function() {
-        chart.append([Math.ceil(Math.random() * 1000 + 400)]);
+        chart.append([Math.ceil(Math.random() * 300 + 50)]);
     }, 300);
 }
--- tgc-lib.js
+++ tgc-lib.js
@@ -3,63 +3,70 @@ var tgc = {};
 (function() {
 
     /**
-     * size: {width: 500, height: 400}
-     * segments: {multiplier: 0.1, countX: 10, minCountY: 10}
-     * style: {
+     * Example of arguments:
+     * size = {width: 1000, height: 400}
+     * point = {width: 50, height: 1}
+     * style = {
      *  background: {color: "#000000", alpha: 0.5},
      *  axis: {thickness: 3, color: "#FF0000", alpha: 1},
-     *  grid: {thickness: 1, color: "#00FFFF", alpha: 1},
+     *  grid: {thickness: 1, color: "#00FFFF", alpha: 1, width: 1, height: 40},
      *  chart: {thickness: 3, radius: 4, color: "#000000", alpha: 0.8}
      * }
      */ 
-    function Chart(size, segments, style) {
+    function StreamingChart(size, point, style) {
         this.Container_constructor();
         
-        this.size = size;
-        this.segments = segments;
-        this.style = style;
+        this._data = [];
         
-        this.data = [];
-        this.maxCountY = 0;
+        this._size = size;
+        this._point = point;
+        this._style = style;
         
-        this.backgroundShape = this.addChild(new createjs.Shape());
-        this.axisShape = this.addChild(new createjs.Shape());
-        this.gridShape = this.addChild(new createjs.Shape());
-        this.chartShape = this.addChild(new createjs.Shape());
+        this._widthSegmentsCount = Math.floor(this._size.width /  this._point.width);
+        
+        this._backgroundShape = this.addChild(new createjs.Shape());
+        this._axisShape = this.addChild(new createjs.Shape());
+        this._gridShape = this.addChild(new createjs.Shape());
+        this._chartShape = this.addChild(new createjs.Shape());
         
         this.updateStyle();
     }
     
-    var p = createjs.extend(Chart, createjs.Container);
+    var p = createjs.extend(StreamingChart, createjs.Container);
     
     //
     // PUBLIC METHODS
     //
     
     p.append = function(data) {
-        var totalData = this.data.concat(data);
-        var stepX = this.size.width / this.segments.countX;
+        var totalData = this._data.concat(data);
         
-        var offset = totalData.length > this.segments.countX ? 0 : Math.max(this.data.length - 1, 0);
-        var offsetX = offset * stepX;
-        var lengthData = Math.min(totalData.length - offset, this.segments.countX + 1);
+        var offset = totalData.length > this._widthSegmentsCount ? 0 : Math.max(this._data.length - 1, 0);
+        var offsetX = offset * this._point.width;
+        var lengthData = Math.min(totalData.length - offset, this._widthSegmentsCount + 1);
         
-        if (!offset) this.chartShape.graphics.clear();
-        console.log("offset " + offset + "; data " + totalData.slice(-lengthData));
+        if (!offset) this._chartShape.graphics.clear();
         
-        this._drawChart(offsetX, stepX, totalData.slice(-lengthData), this.style.chart);
-        this.data = totalData;
+        this._drawChart(offsetX, this._point.width, totalData.slice(-lengthData), this._style.chart);
+        this._data = totalData;
     };
     
     p.clear = function() {
-        this.segments = [];
-        this.chartShape.graphics.clear();
+        this._data = [];
+        this._chartShape.graphics.clear();
+    };
+    
+    p.setStyle = function(style) {
+        this._style = style;
+        this.updateStyle();
     };
     
     p.updateStyle = function() {
-        this._drawBackgroundShape(this.size, this.style.background);
-        this._drawAxisShape(this.size, this.style.axis);
-        this._drawGridShape(this.segments.countX, Math.max(this.segments.minCountY, this.maxCountY), this.style.grid);
+        this._drawBackgroundShape(this._size, this._style.background);
+        this._drawAxisShape(this._size, this._style.axis);
+        var stepX = this._point.width * this._style.grid.width;
+        var stepY = this._point.height * this._style.grid.height;
+        this._drawGridShape(stepX, stepY, this._style.grid);
     };
     
     //
@@ -67,14 +74,14 @@ var tgc = {};
     //
     
     p._drawChart = function(offsetX, stepX, data, style) {
-        var mult = this.segments.multiplier;
+        var multY = this._point.height;
         var aX, aY, bX, bY;
         aX = offsetX;
-        aY = data[0] * mult;
+        aY = data[0] * multY;
         if (!offsetX) this._drawPoint(0, aY, "standart", style);
         for (var i = 0; i < data.length - 1; i++) {
             bX = offsetX + stepX * (i + 1);
-            bY = data[i + 1] * mult;
+            bY = data[i + 1] * multY;
             this._drawSegment(aX, aY, bX, bY, style);
             aX = bX;
             aY = bY;
@@ -82,28 +89,28 @@ var tgc = {};
     };
     
     p._drawSegment = function(aX, aY, bX, bY, style) {
-        var graphics = this.chartShape.graphics;
+        var graphics = this._chartShape.graphics;
         
         graphics.setStrokeStyle(style.thickness, "round");
         graphics.beginStroke(style.color);
-        graphics.moveTo(aX, this.size.height - aY).lineTo(bX, this.size.height - bY);
+        graphics.moveTo(aX, this._size.height - aY).lineTo(bX, this._size.height - bY);
         graphics.endStroke();
         
         this._drawPoint(bX, bY, "standart", style);
     };
     
     p._drawPoint = function(x, y, type, style) {
-        var graphics = this.chartShape.graphics;
+        var graphics = this._chartShape.graphics;
         switch(type) {
         case "selected":
             graphics.setStrokeStyle(style.thickness, "round");
             graphics.beginStroke(style.color);
-            graphics.drawCircle(x, this.size.height - y, style.radius);
+            graphics.drawCircle(x, this._size.height - y, style.radius);
             graphics.endStroke();
             break;
         case "standart":
             graphics.beginFill(style.color);
-            graphics.drawCircle(x, this.size.height - y, style.radius);
+            graphics.drawCircle(x, this._size.height - y, style.radius);
             graphics.endFill();
             break;
         default:
@@ -112,40 +119,37 @@ var tgc = {};
     };
     
     p._drawBackgroundShape = function(size, style) {
-        var graphics = this.backgroundShape.graphics.clear();
+        var graphics = this._backgroundShape.graphics.clear();
         graphics.beginFill(style.color);
         graphics.drawRoundRect(0, 0, size.width, size.height, 3);
-        this.backgroundShape.alpha = style.alpha;
+        this._backgroundShape.alpha = style.alpha;
     };
     
     p._drawAxisShape = function(size, style) {
-        var graphics = this.axisShape.graphics.clear();
+        var graphics = this._axisShape.graphics.clear();
         graphics.setStrokeStyle(style.thickness, "butt").beginStroke(style.color);
         graphics.moveTo(0, 0).lineTo(0, size.height).lineTo(size.width, size.height);
         graphics.endStroke();
-        this.axisShape.alpha = style.alpha;
+        this._axisShape.alpha = style.alpha;
     };
     
-    p._drawGridShape = function(countX, countY, style) {
-        var stepX = this.size.width / countX;
-        var stepY = this.size.height / countY;
-        
-        var graphics = this.gridShape.graphics.clear();
+    p._drawGridShape = function(stepX, stepY, style) {
+        var graphics = this._gridShape.graphics.clear();
         graphics.setStrokeDash([3, 5]);
         graphics.setStrokeStyle(style.thickness, "butt").beginStroke(style.color);
-        for (var x = stepX; x < this.size.width; x += stepX) {
+        for (var x = stepX; x < this._size.width; x += stepX) {
             graphics.moveTo(x, 0);
-            graphics.lineTo(x, this.size.height);
+            graphics.lineTo(x, this._size.height);
         }
-        for (var y = stepY; y < this.size.height; y += stepY) {
+        for (var y = this._size.height - stepY; y > 0; y -= stepY) {
             graphics.moveTo(0, y);
-            graphics.lineTo(this.size.width, y);
+            graphics.lineTo(this._size.width, y);
         }
         graphics.endStroke();
-        this.gridShape.alpha = style.alpha;
+        this._gridShape.alpha = style.alpha;
     };
     
     // ---
     
-    tgc.Chart = createjs.promote(Chart, "Container");
+    tgc.StreamingChart = createjs.promote(StreamingChart, "Container");
 }());
